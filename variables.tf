3ef405913061b1552523eb6323cc68028cebc22e
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-02-08 10:53:13 +0000

    Initial commit. Infra and pipelines
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- variables.tf
+++ variables.tf
@@ -8,3 +8,10 @@ variable "region" {
   type    = string
   default = "eu-west-2"
 }
+
+variable "image_version" {
+  description = "Container tag values."
+  default = {
+    s3-object-tagger = "0.0.3"
+  }
+}
