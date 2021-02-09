922d98c0721cb8689b6fab1bab8983526fde575c
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-02-09 11:39:38 +0000

    Name
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "pdm_object_tagger_batch_config" {
+output "pdm_object_tagger_batch" {
   value = {
     job_queue      = aws_batch_job_queue.pdm_object_tagger
     job_definition = aws_batch_job_definition.pdm_object_tagger
