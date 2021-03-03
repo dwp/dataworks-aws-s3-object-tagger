dcc8391c6cb041083123ff0f11569c8196c5883f
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2021-03-03 14:37:40 +0000

    adding tagging jobs for payment timelines

--- output.tf
+++ output.tf
@@ -16,3 +16,11 @@ output "pdm_object_tagger_data_classification" {
 output "pdm_object_tagger_iam" {
   value = aws_iam_role.pdm_object_tagger
 }
+
+output "pt_object_tagger_data_classification" {
+  value = {
+    config_prefix  = local.config_prefix
+    config_file    = local.config_filename
+    data_s3_prefix = local.pt_s3_prefix
+  }
+}
