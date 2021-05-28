510fc24e058dd96264b5f7a3c3e4d5612a2c6707
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-28 11:03:27 +0100

    Switch to using the new VPCE SG

--- locals.tf
+++ locals.tf
@@ -2,6 +2,7 @@ locals {
   internal_compute_vpc_prefix_list_ids_s3 = data.terraform_remote_state.internal_compute.outputs.vpc.vpc.prefix_list_ids.s3
   internal_compute_subnets                = data.terraform_remote_state.internal_compute.outputs.compute_environment_subnet
   internal_compute_vpc_id                 = data.terraform_remote_state.internal_compute.outputs.vpc.vpc.vpc.id
+  internal_compute_vpce_security_group_id = data.terraform_remote_state.internal_compute.outputs.vpce_security_groups.s3_object_tagger_batch_vpce_security_group.id
 
   s3_object_tagger_image            = "${local.account.management}.${data.terraform_remote_state.aws_ingestion.outputs.vpc.vpc.ecr_dkr_domain_name}/dataworks-s3-object-tagger:${var.image_version.s3-object-tagger}"
   s3_object_tagger_application_name = "s3-object-tagger"
