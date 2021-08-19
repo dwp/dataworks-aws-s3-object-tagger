1ed6f934deee7394ea55e7edfdfdca8dfa296c0e
Connor Avery <55276424+connoravo@users.noreply.github.com>
2021-08-19 11:26:35 +0100

    Pin tagger version per environment, restore UC prefix (#49)
    * Replace version and UC location
    
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
    
    * Make version per env
    
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
    
    * Make locals simpler
    
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
    
    Co-authored-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- locals.tf
+++ locals.tf
@@ -4,11 +4,13 @@ locals {
   internal_compute_vpc_id                 = data.terraform_remote_state.internal_compute.outputs.vpc.vpc.vpc.id
   internal_compute_vpce_security_group_id = data.terraform_remote_state.internal_compute.outputs.vpce_security_groups.s3_object_tagger_batch_vpce_security_group.id
 
-  s3_object_tagger_image            = "${local.account.management}.${data.terraform_remote_state.aws_ingestion.outputs.vpc.vpc.ecr_dkr_domain_name}/dataworks-s3-object-tagger:${var.image_version.s3-object-tagger}"
+  s3_object_tagger_image = "${local.account.management}.${data.terraform_remote_state.aws_ingestion.outputs.vpc.vpc.ecr_dkr_domain_name}/dataworks-s3-object-tagger:${var.image_version.s3-object-tagger[local.environment]}"
   s3_object_tagger_application_name = "s3-object-tagger"
   config_prefix                     = "component/rbac"
   config_filename                   = "data_classification.csv"
-  pdm_s3_prefix                     = "data/uc/uc.db"
+
+//  Include trailing slash to prevent S3 listing items outside of destination
+  pdm_s3_prefix                     = "data/uc/"
   pt_s3_prefix                      = "data/uc_payment_timelines"
   clive_s3_prefix                   = "data/uc_clive"
 
