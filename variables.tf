a63b90dfb9df3b1c822eeec0c16394dda2694117
Connor Avery <55276424+connoravo@users.noreply.github.com>
2021-08-19 16:53:52 +0100

    Bump version (#51)
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
    
    Co-authored-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- variables.tf
+++ variables.tf
@@ -13,10 +13,10 @@ variable "image_version" {
   description = "Container tag values."
   default = {
     s3-object-tagger = {
-      development = "0.0.22"
-      qa          = "0.0.22"
-      integration = "0.0.22"
-      preprod     = "0.0.22"
+      development = "0.0.23"
+      qa          = "0.0.23"
+      integration = "0.0.23"
+      preprod     = "0.0.23"
       production  = "0.0.19"
     }
   }
